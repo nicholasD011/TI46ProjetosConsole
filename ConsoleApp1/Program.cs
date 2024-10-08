787708a8f134b0a5dc4bc808cf7d2715ee574707
Nicholas Davi <djnicolaz10@gmail.com>
2024-10-08 18:21:10 -0300

    Parei no exercicio 8

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,8 +10,50 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("dasdad");
-            Console.Read();
+            Console.Write("Escolha um exercicío da lista01: ");
+           
+            Lista01 lista01 = new Lista01();
+
+            int escolha = Convert.ToInt32(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 1:
+                    lista01.Exercicio01();
+                    break;
+                    case 2:
+                    lista01.Exercicio02();
+                    break;
+                    case 3:
+                    lista01.Exercicio03();
+                    break;
+                    case 4:
+                    lista01.Exercicio04();
+                    break;
+                    case 5:
+                    lista01.Exercicio05();
+                    break;
+                    case 6:
+                    lista01.Exercicio06();
+                    break;
+                    case 7:
+                    lista01.Exercicio07();
+                    break;
+                    case 8:
+                    lista01.Exercicio08();
+                    break;
+                    case 9:
+                    lista01.Exercicio09();
+                    break;
+                    case 10:
+                    lista01.Exercicio10();
+                    break;
+                    
+                    
+
+            }
+
+
         }
     }
 }
